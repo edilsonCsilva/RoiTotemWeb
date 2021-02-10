d2550e7d22ef0542852b90385cb735e3d1c31358
edilsonCsilva <edilsonclaudinosilva@gmail.com>
2021-02-10 13:32:14 -0300

    3-view_de_inicio_10022021_1331

--- index.js
+++ index.js
@@ -2,20 +2,14 @@ const express = require("express")
 const cors    =require("cors")
 const bodyParser = require("body-parser")
 const swig  = require('swig');
-
-
-
 const  app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
-
-
-
-
-
+const title="Still, Roi!"
+ 
 //Inicio rotas do site 
 app.get("/css/:style", function(req,res){
     res.sendFile(__dirname + "/css/"+req.params.style )
@@ -32,22 +26,21 @@ app.get("/img/:img", function(req,res){
 
 app.get("/", function(req,res){
     res.send(swig.renderFile(__dirname+'/views/index.html', {
-        pagename: 'awesome people',
-        authors: ['Paul', 'Jim', 'Jane','Paul', 'Jim', 'Jane']    
-
+        title: title
     }))
   })
 
+
+app.get("/start", function(req,res){
+    res.send(swig.renderFile(__dirname+'/views/start.html', {
+        title: title
+    }))
+  })
+
+
 //Fim rotas do site 
-
-
-
-
-
-
-
-
 
 app.listen(8089,function(){
     console.log("Rodando...")
 })
+ 
